855f550d91767e31ca792b7039d256a849e33917
Gumeniuk Ruslan <bishob1997@gmail.com>
2023-03-18 09:30:02 +0200

    created basic gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+
+function createGalleryElement(el) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+  </a>
+</li>`
+}
+
+const createdGallery = galleryItems.map(createGalleryElement).join('');
+galleryBox.insertAdjacentHTML('beforeend', createdGallery);
